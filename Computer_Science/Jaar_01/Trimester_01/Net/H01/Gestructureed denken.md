f8a2748cf8b7aa2c518268d6186dfbe1a7140307
jacy meert <jacy.meert@student.pxl.be>
2023-09-26 16:56:53 +0200

    update

---
+++ Computer_Science/Jaar_01/Trimester_01/Net/H01/Gestructureed denken.md
@@ -0,0 +1,26 @@
+Gestructureed denken Voorbeeld
+	Fase  1 
+		Gegeven: /
+		Gevraagd: □ Wijzig tekst in "Welkom programmeurs"
+		• Berichtenvenster "Hallo netwerkbeheer"
+		• Wijzig tekst in "Muis zweeft over label"
+	Fase 2 Oplossingsstrategie 
+		• Klik op 'Wijzigen': druk tekst "Welkom programmeurs" in label
+		• Klik op 'Bericht': berichtenvenster "Hallo netwerkbeheer"
+		• Muis over label: druk tekst "Muis zweeft over label" in label
+	Fase 3 Algoritme
+		Begin BtnWijzigen_
+		druk "Welkom programmeurs" (LblHallo)
+		Einde
+		Begin BtnBericht_Click
+		druk "Hallo netwerkbeheer" (berichtenvenster)
+		Einde
+		Begin LblTekst_MouseEnter
+		druk "Muis zweeft over label" (LblHallo)
+		Einde
+	Fase 4 Programme
+	![[Pasted image 20230919155241.png]]
+	Fase 5 testen
+		Testen van de code onder de mogelijke unit testen
+	Fase 6 Documentatie
+		Ontwerp, Probleemanalasy, algoritme
---
+++ Computer_Science/Jaar_01/Trimester_01/Net/H02/BasisVariabelen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasisVariabelen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Geef Getal: "); // Vragen aan de user als die een getal ingeeft
+            double getal1 = Convert.ToDouble(Console.ReadLine()); // Convert van een string wat de user ingeeft naar een double
+
+            Console.Write("Geef Getal: ");
+            double getal2 = Convert.ToDouble(Console.ReadLine());
+
+            double som = getal1 * getal2; // Optellen
+            double aftrekking = getal1 - getal2; // Aftrekken
+            double product = getal2 * getal1; // product
+            double deling = getal1 / getal2;  // deling
+
+            Console.WriteLine("De som, aftrekking, prodcut en deling is: " + som + aftrekking + product + deling);
+
+            Console.ReadKey(); // sluit met een keypress het programma af
+        }
+    }
+}
