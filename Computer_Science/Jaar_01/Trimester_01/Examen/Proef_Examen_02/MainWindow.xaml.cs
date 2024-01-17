e78382b992b26a99b3cb03ba6115e09435527d69
jacymeertPXL <jacy.meert@student.pxl.be>
2024-01-17 15:28:25 +0100

    Update

--- Computer_Science/Jaar_01/Trimester_01/Examen/Proef_Examen_02/MainWindow.xaml.cs
+++ Computer_Science/Jaar_01/Trimester_01/Examen/Proef_Examen_02/MainWindow.xaml.cs
@@ -63,6 +63,88 @@ namespace Proef_Examen_02 {
 
         private void BtnBerekenen_Click(object sender, RoutedEventArgs e)
         {
+            Double Som = 0;
+            int BasisBedrag = 0;
+            if (RdbCompetitielid.IsChecked == true)
+            {
+                Som = +50;
+            }
+
+            double rang = (double)Convert.ToDouble(TxtRang.Text);
+            Som = (100 * 0.05) * rang;
+
+            for (int i = 0; i < 1; i++)
+            {
+                if (RdbCadet.Content.ToString() == lidgeldPerCat[i, 0])
+                {
+                    Som += 170;
+                    BasisBedrag += 170;
+                }
+                if (RdbJunior.Content.ToString() == lidgeldPerCat[i, 0])
+                {
+                    Som += 170;
+                    BasisBedrag += 170;
+                }
+                if (RdbMiniem.Content.ToString() == lidgeldPerCat[i, 0])
+                {
+                    Som += 150;
+                    BasisBedrag += 150;
+                }
+                if (RdbSenior.Content.ToString() == lidgeldPerCat[i, 0])
+                {
+                    Som += 200;
+                    BasisBedrag += 200;
+                }
+                if (RdbPreminiem.Content.ToString() == lidgeldPerCat[i, 0])
+                {
+                    Som += 150;
+                    BasisBedrag += 150;
+                }
+            }
+
+            StringBuilder Informatie = new StringBuilder();
+
+            Informatie.AppendLine("InschrijvingBedrag Voor" + CbNaam.SelectionBoxItem.ToString());
+            Informatie.AppendLine("");
+            Informatie.AppendLine("BasisBedrag" + BasisBedrag);
+            Informatie.Append("Te Betalen" + Som); ;
+            TxtInformatie.Text = Informatie.ToString();
+
+            ////// ////// ////// ////// ////// ////// ////// //////
+
+            double Seconden = Convert.ToDouble(TxtRijd.Text);
+
+            for (int j = 0; j < prognoseVakken.Length; j++)
+            {
+                prognoseVakken[j] = new TextBox
+                {
+                    Background = new SolidColorBrush(Colors.LightGreen),
+                    IsReadOnly = true,
+                    HorizontalAlignment = HorizontalAlignment.Left
+                };
+            }
+       
+            for (int i = 0; i < 6; i++)
+            {
+                prognoseVakken[i].Text = Seconden.ToString();
+
+                Seconden = Seconden - (100 * 0.055 - 0.005 * i);
+
+                int getal;
+                if (int.TryParse(prognoseVakken[i].Text, out getal))
+                {
+                    prognoseVakken[i].Width = getal  * 10;
+                }
+            }
+
+            for(int i = 0;i < 6;i++) {
+                System.Windows.Controls.Label label = new System.Windows.Controls.Label { Content = "Over Jaar" + i };
+                SplInformatie.Children.Add(label);
+                SplInformatie.Children.Add(prognoseVakken[i]);
+            }
+            
+            SplInformatie.Background = new SolidColorBrush(Colors.LightGray);
+            LbInformatie.Content = "Gemiddelde Tijdsprognose in Sec: (Geldig voor nieuwe leden);";
 
         }
     }
